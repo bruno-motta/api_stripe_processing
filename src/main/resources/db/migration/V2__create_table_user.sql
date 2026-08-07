afbaac85159c35cfd857c5bb7d831071f3c4aa0a
Bruno Mota <contatobrunotr@gmail.com>
2026-08-07 00:07:44 -0300

    feat: created schemas flyway payment

---
+++ src/main/resources/db/migration/V2__create_table_user.sql
@@ -0,0 +1,17 @@
+CREATE TABLE payment (
+    id UUID PRIMARY KEY NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id),
+    amount DECIMAL(19,2) NOT NULL,
+    currency VARCHAR(20) NOT NULL,
+    description TEXT,
+    status_payment VARCHAR(20) NOT NULL,
+    payment_method VARCHAR(20) NOT NULL,
+    payment_method_id VARCHAR(255) NOT NULL,
+    gateway_transactio_id VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    retry INTEGER NOT NULL DEFAULT 0
+
+
+
+)
