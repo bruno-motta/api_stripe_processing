962ff518bd5f2b1fb85bb2e6a6de36922ceb9820
Bruno Mota <contatobrunotr@gmail.com>
2026-08-16 01:03:45 -0300

    feat: Created the idempotency key domain and entity, along with the Flyway V3__create...

---
+++ src/main/java/com/github/payment/api/domain/model/IdempotencyKey.java
@@ -0,0 +1,44 @@
+package com.github.payment.api.domain.model;
+
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+public class IdempotencyKey {
+
+    private UUID id;
+    private UUID key; // chave enviada pelo cliente no header
+    private UUID paymentId; // referência ao pagamento criado
+    private String responseBody; //  JSON da resposta original para devolver igual
+    private OffsetDateTime createdAt;
+
+    private IdempotencyKey(){
+
+    }
+
+    public static IdempotencyKey create(UUID key, UUID paymentId, String responseBody){
+        if(key == null){
+            throw new IllegalArgumentException("Idempotency key não pode ser nula.");
+        }
+
+        if(paymentId == null){
+            throw new IllegalArgumentException("Payment ID não pode ser nulo.");
+        }
+
+        if(responseBody == null || responseBody.isBlank()){
+            throw new IllegalArgumentException("Response body não pode ser nulo ou vazio.");
+        }
+
+        IdempotencyKey idempotencyKey = new IdempotencyKey();
+        idempotencyKey.id = UUID.randomUUID();
+        idempotencyKey.key = key;
+        idempotencyKey.paymentId = paymentId;
+        idempotencyKey.responseBody = responseBody;
+        idempotencyKey.createdAt = OffsetDateTime.now();
+
+        return idempotencyKey;
+    }
+
+}
