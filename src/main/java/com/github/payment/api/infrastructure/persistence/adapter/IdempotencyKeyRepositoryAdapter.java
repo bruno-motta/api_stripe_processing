eb5d8859fd67c31d96f70d2acd34d75cd847d653
Bruno Mota <contatobrunotr@gmail.com>
2026-08-20 16:06:51 -0300

    feat: imimplemented idempotency adapter

--- src/main/java/com/github/payment/api/infrastructure/persistence/adapter/IdempotencyKeyRepositoryAdapter.java
+++ src/main/java/com/github/payment/api/infrastructure/persistence/adapter/IdempotencyKeyRepositoryAdapter.java
@@ -1,4 +1,31 @@
 package com.github.payment.api.infrastructure.persistence.adapter;
 
-public class IdempotencyKeyRepositoryAdapter {
+import com.github.payment.api.application.ports.out.IdempotencyKeyRepository;
+import com.github.payment.api.domain.model.IdempotencyKey;
+import com.github.payment.api.infrastructure.persistence.entity.IdempotencyKeyEntity;
+import com.github.payment.api.infrastructure.persistence.mapper.IdempotencyKeyEntityMapper;
+import com.github.payment.api.infrastructure.persistence.repositories.IdempotencyKeyEntityRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class IdempotencyKeyRepositoryAdapter implements IdempotencyKeyRepository {
+
+   private final IdempotencyKeyEntityMapper idempotencyKeyEntityMapper;
+   private final IdempotencyKeyEntityRepository idempotencyKeyRepository;
+
+    @Override
+    public IdempotencyKey save(IdempotencyKey key) {
+        IdempotencyKeyEntity entity = idempotencyKeyEntityMapper.toEntity(key);
+        IdempotencyKeyEntity savedEntity = idempotencyKeyRepository.save(entity);
+        return idempotencyKeyEntityMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<IdempotencyKey> findByKey(UUID key) {
+        return idempotencyKeyRepository.findByKey(key)
+                .map(idempotencyKeyEntityMapper::toDomain);
+    }
 }
