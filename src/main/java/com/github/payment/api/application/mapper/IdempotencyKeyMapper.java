8ff3a032bb6eca2d9cf6981a9f2699cff2e3f794
Bruno Mota <contatobrunotr@gmail.com>
2026-08-20 16:00:45 -0300

    feat: created idempotencyKeyEntityMapper

--- src/main/java/com/github/payment/api/application/mapper/IdempotencyKeyMapper.java
+++
@@ -1,4 +0,0 @@
-package com.github.payment.api.application.mapper;
-
-public class IdempotencyKeyMapper {
-}
---
+++ src/main/java/com/github/payment/api/infrastructure/persistence/mapper/IdempotencyKeyEntityMapper.java
@@ -0,0 +1,4 @@
+package com.github.payment.api.infrastructure.persistence.mapper;
+
+public class IdempotencyKeyMapper {
+}
