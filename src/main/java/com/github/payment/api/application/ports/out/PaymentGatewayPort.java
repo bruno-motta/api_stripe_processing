3f072248a61eb40fc580ab1d5de5d8ea57c7cb33
Bruno Mota <contatobrunotr@gmail.com>
2026-08-24 13:57:57 -0300

    feat: Implemented the parameters for creating payments via Stripe, covering both the interfaces and the StripeAdapter.

---
+++ src/main/java/com/github/payment/api/application/ports/out/PaymentGatewayPort.java
@@ -0,0 +1,20 @@
+package com.github.payment.api.application.ports.out;
+
+
+import com.github.payment.api.application.ports.in.PaymentGatewayResult;
+import com.github.payment.api.domain.model.Payment;
+
+public interface PaymentGatewayPort {
+    /*
+    * Processa a cobrança no gateway externo. Define o que o seu sistema precisa que qualquer gateway de pagamento.
+    * Domínio — não sabe que existe Stripe.
+    *
+    * @param payment Objeto de dóminio com valor, moeda, etc.
+    * @param idempotencyKey Chave de idempotência para evitar cobranças duplicadas na Stripe
+    * @return PaymentGatewayReturn com o status e o ID da transação (ex> pi_3Nxxx...)
+    */
+    PaymentGatewayResult charge(Payment payment, String idempotencyKey);  // cobra
+    // PaymentGatewayResult refund(Payment payment);  // estorna
+
+
+}
---
+++ src/main/java/com/github/payment/api/infrastructure/persistence/adapter/StripeGatewayAdapter.java
@@ -0,0 +1,79 @@
+package com.github.payment.api.infrastructure.persistence.adapter;
+
+import com.github.payment.api.application.ports.in.PaymentGatewayResult;
+import com.github.payment.api.application.ports.out.PaymentGatewayPort;
+import com.github.payment.api.domain.model.Payment;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.net.RequestOptions;
+import com.stripe.param.PaymentIntentCreateParams;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Component
+public class StripeGatewayAdapter implements PaymentGatewayPort {
+
+    @Value("${stripe.api-key}")
+    private String stripeApiKey;
+
+    @PostConstruct
+    private void init(){
+        Stripe.apiKey = stripeApiKey;
+    }
+
+
+    @Override
+    public PaymentGatewayResult charge(Payment payment, String idempotencyKey) {
+        try {
+            //Conversão de BigDecimal para centavos como pede a doc do Stripe (ex: 100.50 -> 10050)
+            long amountInCents = payment.getAmount()
+                    .multiply(BigDecimal.valueOf(100))
+                    .longValue();
+
+            PaymentIntentCreateParams.Builder paramsBuilder = PaymentIntentCreateParams.builder()
+                    .setAmount(amountInCents)
+                    .setCurrency(payment.getCurrency().toLowerCase())
+                    .setDescription(payment.getDescription())
+                    .setConfirm(true) //confirma e tenta cobrar novamente.
+                    .setAutomaticPaymentMethods(
+                            PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
+                                    .setEnabled(true)
+                                    .setAllowRedirects(PaymentIntentCreateParams.AutomaticPaymentMethods.AllowRedirects.ALWAYS)
+                                    .build()
+                    );
+            // Se o pagamento já tiver o ID do metodo (ex: pm_card_visa), vincula
+            if(payment.getPaymentMethodId() != null && !payment.getPaymentMethodId().isBlank()){
+                paramsBuilder.setPaymentMethod(payment.getPaymentMethodId());
+            }
+
+            PaymentIntentCreateParams params = paramsBuilder.build();
+
+            //Configuração de Idempotência do lado da Stripe -> Proteção de Ponta a Ponta.
+            RequestOptions requestOptions = RequestOptions.builder()
+                    .setIdempotencyKey(idempotencyKey)
+                    .build();
+
+            log.info("Enviando cobrança para a stripe com chave de Idempotência: {} ",  idempotencyKey);
+            PaymentIntent intent = PaymentIntent.create(params, requestOptions);
+
+            // verifica o status retornado pela Stripe
+            boolean isSucess = "succeeded".equalsIgnoreCase(intent.getStatus());
+            log.info("Stripe respondeu para intent {}: status={}", intent.getId(), intent.getStatus() );
+
+            return new PaymentGatewayResult(intent.getId(), isSucess);
+
+        } catch (StripeException e) {
+            log.error("Erro ao processar pagamento na Stripe: {}", e.getMessage(), e);
+            // Retorna falha mantendo o chargeId como null ou código do erro
+            return new PaymentGatewayResult(null , false);
+        }
+    }
+
+
+}
